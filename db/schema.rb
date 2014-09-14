e59aa98e1a670505c99dfbb735c9c0776aa05968
Benjamin Winkler <abwinkler999@gmail.com>
2014-09-13 23:25:04 -0400

    Very basic styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912005757) do
+ActiveRecord::Schema.define(version: 20140914032323) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
