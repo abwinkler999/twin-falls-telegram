9864624c7d8f9d162cc51329bdb82bff0f2b584f
Benjamin Winkler <abwinkler999@gmail.com>
2014-09-13 23:29:28 -0400

    Devise install

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # for Devise
+  config.action_mailer.default_url_options = { host: 'http://hidden-dusk-3552.herokuapp.com', port: 80 }
 end
