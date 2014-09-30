a8a964dc3686947e3b50bcc63981e52bd54ec438
Benjamin Winkler <abwinkler999@gmail.com>
2014-09-29 22:33:52 -0400

    Really, really rough implementation of users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140914040126) do
+ActiveRecord::Schema.define(version: 20140930022309) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -28,12 +28,12 @@ ActiveRecord::Schema.define(version: 20140914040126) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20140914040126) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "editor",                 default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
