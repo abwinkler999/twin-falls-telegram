1d4b1be3fe0fd35cbfcc5892f01eb25c43bcf15a
Benjamin Winkler <abwinkler999@gmail.com>
2015-01-29 22:27:57 -0500

    Setting up layout for cyclopedia

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,11 @@
 class EntriesController < ApplicationController
+	layout 'layouts/cyclopedia'
+	def index
+		@entries = Entry.all.order(name: :asc)
+
+		respond_to do |format|
+			format.html
+			format.rss { render layout: false}
+		end
+	end
 end
