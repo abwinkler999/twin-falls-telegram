ea204f645340d14aeea8711795268057b5c9d25d
Benjamin Winkler <abwinkler999@gmail.com>
2015-02-04 19:51:42 -0500

    Initial display of personals without creating new

--- app/models/personal.rb
+++ app/models/personal.rb
@@ -1,3 +1,19 @@
 class Personal < ActiveRecord::Base
 	validates_presence_of :body
+
+	def first_letter
+		self.body.strip.chars.first
+	end
+
+	def rest_of_text
+		self.body.strip[1..-1]
+	end
+
+	def signature
+		if signed
+			"– #{self.signed}"
+		else
+			nil
+		end
+	end
 end
