7352f31dde6eb3900b198062cff1813ecf7e6d96
Benjamin Winkler <abwinkler999@gmail.com>
2015-08-16 16:56:59 -0400

    Default user types created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816195610) do
+ActiveRecord::Schema.define(version: 20150816205440) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -57,6 +57,12 @@ ActiveRecord::Schema.define(version: 20150816195610) do
   end
 
   create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_types", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
