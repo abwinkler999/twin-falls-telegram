1718c8eb2f55adb994002add47dcf53ccff695db
Benjamin Winkler <abwinkler999@gmail.com>
2015-09-20 20:34:39 -0400

    Moved cities to YAML file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,45 +2,9 @@ module ApplicationHelper
 	require 'httparty'
 
   	def pull_weather()
-      location = [:barmouth_light, :elliotville, :twin_falls, :samshire, :spencerville, :bright_plain, :witch_haven, :maen_hir, :powdermill_road, :gravel_barrens, :weeping_hill, :flower_hill].sample
-  		case location
-		  when :barmouth_light
-		    stand_in = "Argentia"
-		    locale = "Barmouth Light"
-  		  when :elliotville
-          stand_in = "Lhasa"
-          locale = "Elliotville"
-        when :twin_falls
-          stand_in = "Aspen"
-          locale = "Twin Falls"
-        when :samshire
-          stand_in = "Salisbury"
-          locale = "Samshire"
-        when :spencerville
-          stand_in = "Shreveport"
-          locale = "Spencerville"
-        when :bright_plain
-          stand_in = "Topeka"
-          locale = "Bright Plain"
-        when :witch_haven
-          stand_in = "Miami"
-          locale = "Witch Haven"
-        when :maen_hir
-          stand_in = "Grimsby"
-          locale = "Maen Hir"
-        when :powdermill_road
-          stand_in = "Luverne"
-          locale = "Powdermill Road"
-        when :gravel_barrens
-          stand_in = "Waldport"
-          locale = "Gravel Barrens"
-        when :weeping_hill
-          stand_in = "Basalt"
-          locale = "Weeping Hill"
-        when :flower_hill
-          stand_in = "Chambersburg"
-          locale = "Flower Hill"
-  		end
+			random_location = WEATHER_CITIES.keys.sample
+			locale = WEATHER_CITIES[random_location]["locale"]
+			stand_in = WEATHER_CITIES[random_location]["stand_in"]
       response = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?q=' + stand_in)
       json = JSON.parse(response.body)
 			icon = json["weather"][0]["icon"]
---
+++ config/initializers/weather.rb
@@ -0,0 +1,1 @@
+WEATHER_CITIES = YAML.load_file('config/weather_cities.yml')
