bd987bfe1c9c792ed2a7677f0d8229e30f02df5c
Benjamin Winkler <abwinkler999@gmail.com>
2015-10-10 19:17:35 -0400

    Making sure API key works in Heroku properly

--- config/initializers/weather.rb
+++ config/initializers/weather.rb
@@ -1,2 +1,6 @@
 WEATHER_CITIES = YAML.load_file('config/weather_cities.yml')
-WEATHER_API = YAML.load_file('config/local_env.yml')['WEATHER_API']["api_key"]
+if Rails.env.production?
+  WEATHER_API = ENV['WEATHER_API']
+else
+  WEATHER_API = YAML.load_file('config/local_env.yml')['WEATHER_API']["api_key"]
+end
