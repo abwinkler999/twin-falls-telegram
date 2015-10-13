a1f08ccfeba08a665e7cc747b09917bd436da9b8
Benjamin Winkler <abwinkler999@gmail.com>
2015-10-12 21:50:01 -0400

    Multiple weather cities solution

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,15 @@
 module ApplicationHelper
 	require 'httparty'
 
-  	def pull_weather()
-			random_location = WEATHER_CITIES.keys.sample
-			locale = WEATHER_CITIES[random_location]["locale"]
-			stand_in = WEATHER_CITIES[random_location]["stand_in"]
-			request_url = "http://api.openweathermap.org/data/2.5/weather?q=#{stand_in}&APPID=#{WEATHER_API}"
+  	def pull_weather(city)
+			request_url = "http://api.openweathermap.org/data/2.5/weather?q=#{city}&APPID=#{WEATHER_API}"
       response = HTTParty.get(request_url)
       json = JSON.parse(response.body)
 			icon = json["weather"][0]["icon"]
   		{temp: kelvin_to_fahrenheit(json["main"]["temp"]).round.to_s,
         conditions: json["weather"][0]["description"],
 				icon_url: "http://openweathermap.org/img/w/" + icon + ".png",
-        location: locale
+        in_game_location: locale
       }
   	end
 
